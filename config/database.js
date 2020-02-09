274a18c4f3105fba1c0fe677ff87bffd9f0e5d1c
Zachary Fero <zfero277@ymail.com>
2020-02-09 10:55:32 -0600

    Heroku restructured server

--- config/database.js
+++ config/database.js
@@ -11,6 +11,6 @@ mongoose.connect(process.env.DATABASE_URL,
 // shortcut to mongoose.connection object
 var db = mongoose.connection;
 
-db.on('connected', function () {
-  console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
-});
+db.once("connected", function() {
+  console.log(`db connected ${db.host} : ${db.port}`);
+})
--- server.js
+++ server.js
@@ -10,10 +10,9 @@ require('dotenv').config()
 require('./config/database')
 require('./config/auth')
 
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(express.json())
-
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/api/users', require('./routes/api/user'));
